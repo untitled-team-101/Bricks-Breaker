e2262d1d9f6cb752d366b47a4482b15fb6968ee6
LEON6156SCOTT <tanmay777leon@gmail.com>
2021-06-27 01:02:21 +0530

    Bricks and frame added

---
+++ js/script.js
@@ -0,0 +1,12 @@
+let brick_container = document.querySelector('.brick-container');
+
+function makingBricks(n)
+{
+    for(let i=0;i<n;i++)
+    {
+        const brick = document.createElement('div');
+        brick.className = 'brick';
+        brick_container.appendChild(brick);
+    }
+}
+makingBricks(30);
