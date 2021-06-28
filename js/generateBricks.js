2f482ad713cefdee7838847c8c3f0f8bfb576cea
LEON6156SCOTT <tanmay777leon@gmail.com>
2021-06-28 13:33:38 +0530

    Levels Added to Game
    1. 5 game levels added.
    2. "power assignment to broken bricks" bug fixed

--- js/generateBricks.js
+++ js/generateBricks.js
@@ -19,84 +19,114 @@ function generateFakeBricks(n) {
         brick_container.appendChild(brick);
     }
 }
-
-function generateLevelZero()
-{
-    generateFakeBricks(2);
-    generateBricks(2,'l4');
-    generateFakeBricks(2);
-    generateFakeBricks(1);
-    generateBricks(1,'l1');
-    generateFakeBricks(2);
-    generateBricks(1,'l1');
-    generateFakeBricks(1);
-    generateBricks(2,'l2');
-    generateFakeBricks(2);
-    generateBricks(2,'l2');
-    generateBricks(2,'l3');
-    generateFakeBricks(2);
-    generateBricks(2,'l3');
-    generateBricks(2,'l2');
-    generateFakeBricks(2);
-    generateBricks(2,'l2');
-    generateFakeBricks(1);
-    generateBricks(1,'l1');
-    generateFakeBricks(2);
-    generateBricks(1,'l1');
-    generateFakeBricks(1);
-    generateFakeBricks(2);
-    generateBricks(2,'l4');
-    generateFakeBricks(2);
+function generateSolidBricks(n){
+    for (let i = 0; i < n; i++)
+    {
+        const brick = document.createElement('div');
+        brick.classList.add('brick');
+        brick.classList.add('solid');
+        brick_container.appendChild(brick);
+    }
 }
 
 function generateLevelOne()
 {
-    generateBricks(6,'l3');
-    generateBricks(12,'l4');
-    generateBricks(6,'l2');
-    generateBricks(12,'l1');
+generateBricks(10,'l4');
+generateBricks(10,'l3');
+generateBricks(10,'l2');
+generateBricks(10,'l1');
+generateSolidBricks(2);
+generateFakeBricks(6);
+generateSolidBricks(2);
+generateBricks(10,'l1');
+generateFakeBricks(10);
+generateBricks(10,'l2');
+
 }
 
-function generateLevelTwo(){
-
-    generateFakeBricks(6);
-    generateFakeBricks(2);
-    generateBricks(2,'l3');
-    generateFakeBricks(2);
-    generateBricks(6,'l4');
-    generateFakeBricks(1);
-    generateBricks(4,'l1');
-    generateFakeBricks(1);
-    generateFakeBricks(6);
-    generateBricks(1,'l3')
-    generateFakeBricks(1);
-    generateBricks(1,'l1');
-    generateBricks(1,'l1')
-    generateFakeBricks(1);
-    generateBricks(1,'l3');
-    generateFakeBricks(6);
-    generateBricks(6,'l2');
-    generateBricks(6,'l4');
-    generateBricks(6,'l2');
+function generateLevelTwo()
+{
+    generateBricks(10,'l1');
+    generateBricks(10,'l4');
+    generateFakeBricks(10);
+    generateBricks(10,'l2');
+    generateFakeBricks(10);
+    generateBricks(10,'l3');
+    generateBricks(10,'l1');
 }
 
 function generateLevelThree(){
-    generateBricks(6,'l2');
 
-    generateBricks(2,'l4');
+    generateFakeBricks(10);
+    generateFakeBricks(3);
+    generateBricks(4,'l3');
+    generateFakeBricks(3);
+    generateBricks(3,'l4');
+    generateFakeBricks(4);
+    generateBricks(3,'l1');
+    generateFakeBricks(10);
+    generateBricks(2,'l3');
+    generateBricks(2,'l2')
     generateFakeBricks(2);
-    generateBricks(2,'l4');
-    generateFakeBricks(6);
-    generateBricks(6,'l1');
-    generateFakeBricks(6);
-    generateBricks(12,'l3');
-    generateFakeBricks(6);
-    generateBricks(1,'l1');
-    generateBricks(1,'l2');
-    generateBricks(1,'l3');
-    generateBricks(1,'l3');
-    generateBricks(1,'l2');
-    generateBricks(1,'l1');
+    generateBricks(2,'l2');
+    generateBricks(2,'l3');
+    generateBricks(10,'l4');
+    generateBricks(10,'l1');
+    generateFakeBricks(10);
+    generateBricks(10,'l4')
+    generateBricks(10,'l2')
 }
 
-generateLevelZero();
+function generateLevelFour(){
+    generateBricks(10,'l1');
+    generateFakeBricks(1);
+    generateBricks(2,'l4');
+    generateFakeBricks(4);
+    generateBricks(2,'l4');
+    generateFakeBricks(1);
+    generateBricks(10,'l3');
+    generateFakeBricks(1);
+    generateBricks(2,'l2');
+    generateFakeBricks(4);
+    generateBricks(2,'l2');
+    generateFakeBricks(1);
+    generateBricks(10,'l4');
+    generateBricks(10,'l1');
+    generateFakeBricks(10);
+    generateBricks(10,'l4')
+}
+
+
+function generateLevelFive(){
+    generateFakeBricks(1);
+    generateBricks(1,'l4');
+    generateFakeBricks(6);
+    generateBricks(1,'l4');
+    generateFakeBricks(3);
+    generateBricks(1,'l4');
+    generateFakeBricks(4);
+    generateBricks(1,'l4');
+    generateFakeBricks(3);
+    generateBricks(8,'l4');
+    generateFakeBricks(1);
+    generateBricks(3,'l4');
+    generateSolidBricks(1);
+    generateBricks(2,'l4');
+    generateSolidBricks(1);
+    generateBricks(3,'l4');
+    generateBricks(10,'l4');
+    generateFakeBricks(2);
+    generateBricks(7,'l4');
+    generateFakeBricks(1);
+    generateFakeBricks(2);
+    generateBricks(1,'l4');
+    generateFakeBricks(5,'l4');
+    generateBricks(1,'l4');
+    generateFakeBricks(1);
+    generateFakeBricks(3);
+    generateBricks(2,'l4');
+    generateFakeBricks(1);
+    generateBricks(2,'l4');
+
+}
+generateLevelOne();
--- js/powerUp.js
+++ js/powerUp.js
@@ -1,15 +1,20 @@
 let brick = document.querySelectorAll('.brick');
-let number = [1,2,3,4,5,6,7,8,9,0,10,11,12];
+let number = [1,2,3,4,5,6,7,8,9,0,10];
 
 function generatePowers(){
     for(let i of brick) {
         let numb = number[Math.floor(Math.random() * number.length)];
         if (numb === 0) {
-            i.classList.add('power');
-            i.classList.add('bigBall');
+            if(!(i.classList.contains('broken')) && !(i.classList.contains('solid')))
+            {
+                i.classList.add('power');
+                i.classList.add('bigBall');
+            }
         } else if (numb === 1) {
-            i.classList.add('power');
-            i.classList.add('bigPad');
+            if(!(i.classList.contains('broken')) && !(i.classList.contains('solid'))) {
+                i.classList.add('power');
+                i.classList.add('bigPad');
+            }
         }
     }
 }
